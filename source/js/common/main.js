5f0116cafe7c8bb2e92cd367364156d36c726f09
kamcha7ka <sledsib@ya.ru>
2020-03-06 10:54:19 +0300

    2.00 - [source] Добавит верстку и стилизацию шапки сайта

--- source/js/common/main.js
+++ source/js/common/main.js
@@ -2,7 +2,41 @@
 
 (function () {
   var initPage = function () {
+    var nojs = document.querySelector('.nojs');
+    nojs.classList.remove('nojs');
 
+    // HANDLERS - start
+    var navToggleHandler = function (ntEvt) {
+      ntEvt.preventDefault();
+      var nav = document.querySelector('.nav-main');
+
+      if (ntEvt.target.classList.contains('header__nav-toggle')) {
+        if (ntEvt.target.classList.contains('header__nav-toggle_closed')) {
+          ntEvt.target.classList.remove('header__nav-toggle_closed');
+          ntEvt.target.classList.add('header__nav-toggle_opened');
+          nav.classList.remove('nav-main_closed');
+          nav.classList.add('nav-main_opened');
+          return;
+        }
+        if (ntEvt.target.classList.contains('header__nav-toggle_opened')) {
+          ntEvt.target.classList.remove('header__nav-toggle_opened');
+          ntEvt.target.classList.add('header__nav-toggle_closed');
+          nav.classList.remove('nav-main_opened');
+          nav.classList.add('nav-main_closed');
+          return;
+        }
+      }
+    };
+    // HANDLERS - end
+
+    var controlItems = {
+      nav: 'header__nav-toggle'
+    };
+
+    var navToggle = document.querySelector('.' + controlItems.nav);
+    if (navToggle) {
+      navToggle.addEventListener('click', navToggleHandler);
+    }
   };
 
   window.addEventListener('load', initPage);
