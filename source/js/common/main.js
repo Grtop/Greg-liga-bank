6a5ea97a4d0bfad431ecdecd40566722efff3b47
kamcha7ka <sledsib@ya.ru>
2020-03-08 20:44:32 +0300

    3.01 - [source] Добавит верстку и стилизацию блоков SLIDER и SERVICES

--- source/js/common/main.js
+++ source/js/common/main.js
@@ -5,6 +5,41 @@
     var nojs = document.querySelector('.nojs');
     nojs.classList.remove('nojs');
 
+    // polyfill for forEach - https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Polyfill
+    if (!Array.prototype.forEach) {
+      Array.prototype.forEach = function (callback, thisArg) {
+        var T, k;
+        if (this === null) {
+          throw new TypeError(' this is null or not defined');
+        }
+        var O = Object(this);
+        var len = O.length >>> 0;
+        if (typeof callback !== 'function') {
+          throw new TypeError(callback + ' is not a function');
+        }
+        if (arguments.length > 1) {
+          T = thisArg;
+        }
+        k = 0;
+        while (k < len) {
+          var kValue;
+          if (k in O) {
+            kValue = O[k];
+            callback.call(T, kValue, k, O);
+          }
+          k++;
+        }
+      };
+    }
+
+    // for swiper.js - startsWidth
+    if (!String.prototype.startsWith) {
+      String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+      };
+    }
+
     // HANDLERS - start
     var navToggleHandler = function (ntEvt) {
       ntEvt.preventDefault();
@@ -27,15 +62,80 @@
         }
       }
     };
+
+    var tabClickHandler = function (tiEvt) {
+      tiEvt.preventDefault();
+
+      tabItems.forEach(function (item) {
+        item.parentNode.classList.remove(elemItems.tab + '_active');
+      });
+
+      var currentTab = tiEvt.currentTarget;
+      if (currentTab.classList.contains(controlItems.tab)) {
+        currentTab.parentNode.classList.add(elemItems.tab + '_active');
+      }
+    };
     // HANDLERS - end
 
+    var TABLET_SIZE = 1023;
+
     var controlItems = {
-      nav: 'header__nav-toggle'
+      nav: 'header__nav-toggle',
+      tab: 'service-item__head'
+    };
+
+    var elemItems = {
+      tabList: 'services__list',
+      tab: 'service-item'
     };
 
     var navToggle = document.querySelector('.' + controlItems.nav);
     if (navToggle) {
       navToggle.addEventListener('click', navToggleHandler);
+    }
+
+    if (document.body.offsetWidth > TABLET_SIZE) {
+      var tabItems = document.querySelectorAll('.' + controlItems.tab);
+      if (tabItems.length > 0) {
+        tabItems.forEach(function (item) {
+          item.addEventListener('click', tabClickHandler);
+        });
+      }
+    } else {
+      var tabWrapper = document.querySelector('.' + elemItems.tabList);
+      if (tabWrapper) {
+        tabWrapper.classList.add('swiper-wrapper');
+      }
+      var tabList = document.querySelectorAll('.' + elemItems.tab);
+      if (tabList.length > 0) {
+        tabList.forEach(function (tab) {
+          tab.classList.remove(elemItems.tab + '_active');
+          tab.classList.add('swiper-slide');
+        });
+      }
+    }
+
+    // SLIDER - start
+    if (document.querySelector('.slider')) {
+      var sliderMain = new Swiper('.slider', {
+        loop: true,
+        pagination: {
+          el: '.slider-pagination',
+          type: 'bullets',
+          bulletElement: 'button',
+          clickable: true,
+          bulletClass: 'slider-pagination__item',
+          bulletActiveClass: 'slider-pagination__item_active'
+        },
+      });
+    }
+
+    if (document.body.offsetWidth <= TABLET_SIZE) {
+      if (document.querySelector('.services')) {
+        var sliderService = new Swiper('.services', {
+          loop: true
+        });
+      }
     }
   };
 
