e8b18e7994cc3ea20b3764e4c7bbcee6988d24ee
Greg <grtop@yandex.ru>
2024-02-09 23:30:52 +0300

    стили и подключение к html карты отделений

---
+++ src/components/map/map.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './map-styles.css';
+
+export default function Map() {
+
+  return (
+    <section id="offices" className="main-map container">
+      <h2>Отделения Лига Банка</h2>
+      <img alt="Карта с отделениями" src="./img/map.webp" width="1170" height="462"/>
+    </section>
+  );
+}
