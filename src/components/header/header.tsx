a0ac9a5d5d5d1c86974a2ba8fc363ba23f6cb603
Greg <grtop@yandex.ru>
2024-02-09 23:37:29 +0300

    описание головной части сайта и вход в админ

---
+++ src/components/header/header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../common/logo';
+import './header-styles.css';
+import NavLinks from './nav-links';
+import UserCabinetBtn from './user-cabinet-btn';
+
+interface HeaderProps {
+  setIsModalOpen: (arg: boolean) => void,
+}
+
+export default function Header({setIsModalOpen}: HeaderProps) {
+
+  return (
+    <header>
+      <nav className="header-nav container">
+        <Logo/>
+        <NavLinks/>
+        <UserCabinetBtn setIsModalOpen={setIsModalOpen} />
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/header/user-cabinet-btn.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { UserCabinetIcon } from '../icons';
+
+export default function UserCabinetBtn({setIsModalOpen}: {setIsModalOpen: (arg: boolean) => void}) {
+
+  return (
+    <button
+      className="header-nav__cabinet"
+      aria-label="Войти в интернет-банк"
+      onClick={() => setIsModalOpen(true)}
+    >
+      <span>
+        <UserCabinetIcon />
+      </span>
+      <span className="header-nav__cabinet-text">Войти в Интернет-банк</span>
+    </button>
+  );
+}
