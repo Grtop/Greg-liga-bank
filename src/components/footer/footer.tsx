5425fee392d7706cd2040bc463f32177b7ecc951
Greg <grtop@yandex.ru>
2024-02-09 23:45:23 +0300

    подключение к html странице подвала

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from '../../common/logo';
+import { footerText } from '../../utils/const';
+import FooterLinks from './footer-links';
+import FooterPhone from './footer-phone';
+import FooterSms from './footer-sms';
+import FooterSocial from './footer-social';
+import './footer-styles.css';
+
+export default function Footer() {
+
+  return (
+    <div className="wrapper">
+      <footer className="footer container">
+        <div className="footer__logo">
+          <Logo/>
+          <address>
+            {footerText.address}
+          </address>
+        </div>
+        <FooterLinks/>
+        <FooterSms/>
+        <FooterPhone/>
+        <FooterSocial/>
+      </footer>
+    </div>
+  );
+}
